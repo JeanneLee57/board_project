f3be121a948e3bdf8044d4ec1a8f6ae7c1da3b71
JeanneLee57 <jinhwa.lee.57@gmail.com>
2023-06-08 15:25:54 +0900

    add prev and next button to pagination

--- app/board/page.tsx
+++ app/board/page.tsx
@@ -2,28 +2,35 @@ import ItemList from "./ItemList";
 import Button from "@/components/Button";
 import { connectDB } from "@/util/database";
 import { Item } from "./ItemList";
+import { Prev, Next } from "@/components/Button";
 
 export const ITEMS_PER_PAGE = 10;
+export const PAGES_PER_SKIP = 5;
+export const PAGES_RANGE = Math.floor(PAGES_PER_SKIP / 2);
 
 export default async function Board(props: {
   searchParams: { page?: string };
 }) {
-  const page = props.searchParams.page;
   const client = await connectDB;
   const db = client.db("forum");
   const itemCount = await db.collection("post").countDocuments();
   const pageNum = Math.ceil(itemCount / ITEMS_PER_PAGE);
+  const currentPage = props.searchParams.page ? +props.searchParams.page : 1;
+  const startPage = Math.max(currentPage - PAGES_RANGE, 1);
+  const baseEndPage =
+    currentPage < 1 + PAGES_RANGE ? PAGES_PER_SKIP : currentPage + PAGES_RANGE;
+  const endPage = Math.min(baseEndPage, pageNum);
   const divs = [];
-  for (let i = 1; i <= pageNum; i++) {
-    divs.push(<Button count={i} />);
+  for (let i = startPage; i <= endPage; i++) {
+    divs.push(<Button count={i} currentPage={currentPage} />);
   }
 
-  const items = page
+  const items = currentPage
     ? await db
         .collection<Item>("post")
         .find()
         .sort({ date: -1 })
-        .skip(ITEMS_PER_PAGE * (+page - 1))
+        .skip(ITEMS_PER_PAGE * (currentPage - 1))
         .limit(ITEMS_PER_PAGE)
         .toArray()
     : await db
@@ -39,7 +46,11 @@ export default async function Board(props: {
         전체 글
       </h1>
       <ItemList items={items} />
-      <div className="flex flex-row justify-center gap-x-8">{divs}</div>
+      <div className="flex flex-row justify-center items-center gap-x-8 mt-16">
+        {startPage > 1 && <Prev page={currentPage} />}
+        {divs}
+        {endPage < pageNum && <Next page={currentPage} pageNum={pageNum} />}
+      </div>
     </main>
   );
 }
--- components/Button.tsx
+++ components/Button.tsx
@@ -1,12 +1,25 @@
 "use client";
 import { useRouter } from "next/navigation";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChevronLeft } from "@fortawesome/free-solid-svg-icons";
+import { faChevronRight } from "@fortawesome/free-solid-svg-icons";
+import "@fortawesome/fontawesome-svg-core/styles.css";
 
-export default function Button(props: { category?: string; count: number }) {
+const PAGES_PER_SKIP = 5;
+const PAGES_RANGE = Math.floor(PAGES_PER_SKIP / 2);
+
+export default function Button(props: {
+  category?: string;
+  count: number;
+  currentPage: number;
+}) {
   const router = useRouter();
 
   return (
     <button
-      className="text-xl hover:scale-105 active:translate-y-0.5"
+      className={`${
+        props.count === props.currentPage ? "text-black" : "text-gray-500"
+      } text-xl hover:scale-105 active:translate-y-0.5`}
       onClick={() =>
         router.push(
           `/board${props.category ? "/" + props.category : ""}?page=${
@@ -16,6 +29,54 @@ export default function Button(props: { category?: string; count: number }) {
       }
     >
       {props.count}
+    </button>
+  );
+}
+
+export function Prev(props: { category?: string; page: number }) {
+  const router = useRouter();
+
+  return (
+    <button
+      className="text-xl hover:scale-105 active:translate-y-0.5"
+      onClick={() => {
+        if (props.page - PAGES_RANGE > 0) {
+          router.push(
+            `/board${props.category ? "/" + props.category : ""}?page=${
+              props.page - PAGES_PER_SKIP > 0 ? props.page - PAGES_PER_SKIP : 1
+            }`
+          );
+        }
+      }}
+    >
+      <FontAwesomeIcon icon={faChevronLeft} className="text-sm" />
+    </button>
+  );
+}
+
+export function Next(props: {
+  category?: string;
+  page: number;
+  pageNum: number;
+}) {
+  const router = useRouter();
+
+  return (
+    <button
+      className="text-xl hover:scale-105 active:translate-y-0.5"
+      onClick={() => {
+        if (props.page + PAGES_RANGE <= props.pageNum) {
+          router.push(
+            `/board${props.category ? "/" + props.category : ""}?page=${
+              props.page + PAGES_PER_SKIP < props.pageNum
+                ? props.page + PAGES_PER_SKIP
+                : props.pageNum
+            }`
+          );
+        }
+      }}
+    >
+      <FontAwesomeIcon icon={faChevronRight} className="text-sm" />
     </button>
   );
 }
