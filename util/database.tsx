b28c049cdb850d8d223132b7080cd6a7e95cbc9e
JeanneLee57 <jinhwa.lee.57@gmail.com>
2023-08-22 16:16:17 +0900

    fix: cannot find global 에러 해결

--- util/database.tsx
+++ util/database.tsx
@@ -5,10 +5,10 @@ const options: any = { useNewUrlParser: true };
 let connectDB: Promise<MongoClient>;
 
 if (process.env.NODE_ENV === "development") {
-  if (!global._mongo as any) {
-    global._mongo = new MongoClient(url, options).connect();
+  if (!globalThis._mongo) {
+    globalThis._mongo = new MongoClient(url, options).connect();
   }
-  connectDB = global._mongo;
+  connectDB = globalThis._mongo;
 } else {
   connectDB = new MongoClient(url, options).connect();
 }
