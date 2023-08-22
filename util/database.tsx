e10329ae2b6c030713fe2586a55ca1267e8d3153
JeanneLee57 <jinhwa.lee.57@gmail.com>
2023-08-22 18:56:28 +0900

    fix: fix deploy error

--- util/database.tsx
+++ util/database.tsx
@@ -3,7 +3,11 @@ import { MongoClient } from "mongodb";
 const url = process.env.DB_CONN_STRING as string;
 const options: any = { useNewUrlParser: true };
 let connectDB: Promise<MongoClient>;
-
+declare global {
+  namespace globalThis {
+    var _mongo: Promise<MongoClient>;
+  }
+}
 if (process.env.NODE_ENV === "development") {
   if (!globalThis._mongo) {
     globalThis._mongo = new MongoClient(url, options).connect();
